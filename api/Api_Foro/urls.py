edd9990898b6c79054d64a73eccc5ad70efdcb51
Juangr1803 <j3045771061@gmail.com>
2020-11-18 22:50:53 -0500

    [ADD] Backend

---
+++ api/Api_Foro/urls.py
@@ -0,0 +1,11 @@
+# Django
+from django.contrib import admin
+from django.urls import path,include
+from interests.urls import router
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path('api/', include(router.urls)),
+]
