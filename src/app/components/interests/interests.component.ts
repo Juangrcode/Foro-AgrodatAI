8a95aa9cac0da83dfd70a2f780f249bd472d6c19
Juangr1803 <j3045771061@gmail.com>
2020-11-20 16:42:09 -0500

    [ADD] Interests

--- src/app/components/interests/interests.component.ts
+++ src/app/components/interests/interests.component.ts
@@ -1,42 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { InterestsService } from '../../services/interests.service';
+// import { InterestsService } from '../../services/interests.service';
 
 @Component({
   selector: 'app-interests',
   templateUrl: './interests.component.html',
   styleUrls: ['./interests.component.scss'],
-  providers: [InterestsService],
+  // providers: [InterestsService],
 })
 export class InterestsComponent implements OnInit {
-  post: Boolean = false;
+  // post: Boolean = false;
 
-  constructor(public interestsService: InterestsService) {}
+  // constructor(public interestsService: InterestsService) {}
 
   ngOnInit(): void {
-    this.getAllInterests();
+    // this.getAllInterests();
   }
 
-  getAllInterests() {
-    this.interestsService.getAllInterests().subscribe(
-      (res) => {
-        this.interestsService.interests = res;
-      },
-      (err) => console.error(err)
-    );
-  }
+  // getAllInterests() {
+  //   this.interestsService.getAllInterests().subscribe(
+  //     (res) => {
+  //       this.interestsService.interests = res;
+  //     },
+  //     (err) => console.error(err)
+  //   );
+  // }
 
-  addInterest(id: any) {
-    console.log(id);
-    console.log(this.interestsService.getInterest(id));
-    this.interestsService.getInterest(id).subscribe(
-      (res) => {
-        console.log(res);
-      },
-      (err) => console.error(err)
-    );
-  }
+  // addInterest(id: any) {
+  //   console.log(id);
+  //   console.log(this.interestsService.getInterest(id));
+  //   this.interestsService.getInterest(id).subscribe(
+  //     (res) => {
+  //       console.log(res);
+  //     },
+  //     (err) => console.error(err)
+  //   );
+  // }
 
-  seePost() {
-    this.post = true;
-  }
+  // seePost() {
+  //   this.post = true;
+  // }
 }
