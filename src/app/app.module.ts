542cb74b334f6fc6b30258d7eb308a9af6bc4c1f
Ana Mongui <anamariamongui@gmail.com>
2020-11-20 21:22:46 -0500

    Cambios Iniciales Loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaquetaComponent } from './maqueta/maqueta.component';
 import { NoticiaComponent } from './noticia/noticia.component';
 import { ProveedorComponent } from './proveedor/proveedor.component';
 import { ProductorComponent } from './productor/productor.component';
-<<<<<<< HEAD
-import { LoaderComponent } from './loader/loader.component';
-=======
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
-
-
->>>>>>> e6aa3e329468e44780739433cfce0a59e2c1df32
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   declarations: [
