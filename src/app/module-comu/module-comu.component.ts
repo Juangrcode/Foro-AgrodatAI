710fcbe4538bc3e5b5d4899d4e6ac67e188183c1
andresloaiza722@gmail.com <andresloaiza722@gmail.com>
2020-12-02 10:44:32 -0500

    ADD-omunidades8

--- src/app/module-comu/module-comu.component.ts
+++ src/app/module-comu/module-comu.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2'
+// import Swal from 'sweetalert2/dist/sweetalert2.js'
 
 @Component({
   selector: 'app-module-comu',
   templateUrl: './module-comu.component.html',
   styleUrls: ['./module-comu.component.scss']
 })
-export class ModuleComuComponent implements OnInit {
-comunidades:boolean=false
-crear:boolean=false
-formulario:boolean=false
-foro:boolean=false
-  constructor() { }
+export class ModuleComuComponent  {
+  comunidades:boolean=false
+  crear:boolean=false
+  foro:boolean=false
+  filterName:string
+  constructor() { } 
 
-  ngOnInit(): void {
+  guardar(){
+    Swal.fire({ 
+      icon: 'success',
+      title: 'gracias',
+      showConfirmButton: true,
+      
+    }).then(function () {
+        console.log ('Se hizo clic en el botón Aceptar.');
+      });
+    
   }
+  addPost(form: NgForm){
+    form.resetForm(); // or form.reset();
 
+    // console.log(form)
+  }
 }
